395885c5bd4396e1aad72ba0c80c63431e96d245
Shiza Batool <91551269+Shiza-Batool@users.noreply.github.com>
2022-12-27 08:39:11 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,75 @@
+import threading
+
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+ 
+    L = [0] * (n1)
+    R = [0] * (n2)
+ 
+    for i in range(0 , n1):
+        L[i] = arr[l + i]
+ 
+    for j in range(0 , n2):
+        R[j] = arr[m + 1 + j]
+ 
+    i = 0    
+    j = 0    
+    k = l    
+ 
+    while i < n1 and j < n2 :
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+ 
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+ 
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def merge_sort(arr, l, r):
+    if l < r:
+        m = (l+(r-1))//2
+ 
+        t1 = threading.Thread(target=merge_sort, args=(arr, l, m))
+        t2 = threading.Thread(target=merge_sort, args=(arr, m+1, r))
+        t3 = threading.Thread(target=merge_sort, args=(arr, m+1, r))
+        t4 = threading.Thread(target=merge_sort, args=(arr, m+1, r))
+        
+        t1.start()
+        t2.start()
+        t3.start()
+        t4.start()
+ 
+        
+        
+        
+        t1.join()
+        t2.join()
+        t3.join()
+        t4.join()
+        
+        merge(arr, l, m, r)
+
+def sort(arr):
+    l = 0
+    r = len(arr) - 1
+    merge_sort(arr, l, r)
+
+
+
+
+# Test the multithreaded merge sort function
+
+arr = [12, 11, 13, 5, 6, 7]
+sort(arr)
+print(arr)
